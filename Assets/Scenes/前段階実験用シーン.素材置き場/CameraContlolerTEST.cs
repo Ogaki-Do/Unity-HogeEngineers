d9bba20a94440b4e169d635249846a684f4fda25
Ogaki-Do <40033136+Ogaki-Do@users.noreply.github.com>
2018-12-01 17:20:41 +0900

    2018_12_1var1
    各種実証実験環境構築

---
+++ Assets/Scenes/前段階実験用シーン.素材置き場/CameraContlolerTEST.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraContlolerTEST : MonoBehaviour {
+    [SerializeField]
+    public Vector3 Center;//カメラの見ている先
+    Transform Thistrans;//その名の通り
+
+    [SerializeField]
+    bool CamMove=false;//カメラが移動したときtrue
+	// Use this for initialization
+	void Start () {
+        //各種初期化取得
+        Thistrans = this.transform;
+        Center = new Vector3(0, 0, 0);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (CamMove) {
+            Thistrans.LookAt(Center);
+        }
+	}
+}
---
+++ Assets/Scenes/前段階実験用シーン.素材置き場/Inspectortest.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;//インスペクタを改造するマン
+
+[CustomEditor(typeof(Inspectortest/*ここは自分のクラス名*/))]
+public class Inspectortest : MonoBehaviour {
+    [SerializeField]
+    int[] a;
+    [SerializeField]
+    Color b;
+    [SerializeField, Range(0, 10)]
+    int c;
+    [SerializeField, Tooltip("解説")]
+    int d;
+    [SerializeField, Multiline(4)]
+    string e;
+
+
+    //ここからUnityEditor必須な
+    
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
